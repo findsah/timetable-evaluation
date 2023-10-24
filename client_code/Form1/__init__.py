a9b5e692fdd6f15c997ed4140248bc7d96604c83
aech046@gmail.com <aech046@gmail.com>
2023-10-24 20:30:43 +0000

    Edited settings and Form1

--- client_code/Form1/__init__.py
+++ client_code/Form1/__init__.py
@@ -7,3 +7,8 @@ class Form1(Form1Template):
     self.init_components(**properties)
 
     # Any code you write here will run before the form opens.
+
+  def radio_button_2_clicked(self, **event_args):
+    """This method is called when this radio button is selected"""
+    pass
+
