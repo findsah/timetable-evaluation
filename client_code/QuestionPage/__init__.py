744b56bb13ac7fff833c54256231ced56f8ca068
aech046@gmail.com <aech046@gmail.com>
2023-11-19 20:56:19 +0000

    Created Form1, ShareLogo.jpg, aum-english-logo1-1.png, unnamed.jpg and 1 more; Edited theme/parameters.yaml, WelcomePage and QuestionPage

--- client_code/QuestionPage/__init__.py
+++ client_code/QuestionPage/__init__.py
@@ -1,6 +1,8 @@
 from ._anvil_designer import QuestionPageTemplate
 from anvil import *
 
+from ..WelcomePage import WelcomePage
+
 class QuestionPage(QuestionPageTemplate):
   def __init__(self, **properties):
     # Set Form properties and Data Bindings.
@@ -11,4 +13,9 @@ class QuestionPage(QuestionPageTemplate):
   def radio_button_2_clicked(self, **event_args):
     """This method is called when this radio button is selected"""
     pass
+
+  def link_1_click(self, **event_args):
+    """This method is called when the link is clicked"""
+    self.content_panel.clear()
+    self.content_panel.add_component(WelcomePage())
 
--- client_code/WelcomePage/__init__.py
+++ client_code/WelcomePage/__init__.py
@@ -7,3 +7,7 @@ class WelcomePage(WelcomePageTemplate):
     self.init_components(**properties)
 
     # Any code you write here will run before the form opens.
+
+  def button_1_click(self, **event_args):
+    """This method is called when the button is clicked"""
+    open_form("QuestionPage")
