d7b9baf8b09f6036bb8283dca7784ce7faaf445f
aech046@gmail.com <aech046@gmail.com>
2023-12-09 19:41:38 +0000

    Edited ContactUsPage

--- client_code/ContactUsPage/__init__.py
+++ client_code/ContactUsPage/__init__.py
@@ -2,9 +2,15 @@ from ._anvil_designer import ContactUsPageTemplate
 from anvil import *
 import anvil.server
 
+from ..WelcomePage import WelcomePage
+
 class ContactUsPage(ContactUsPageTemplate):
   def __init__(self, **properties):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
 
     # Any code you write here will run before the form opens.
+
+  def link_1_click(self, **event_args):
+    """This method is called when the link is clicked"""
+    pass
