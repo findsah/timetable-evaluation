a53de5488ea123e0f10f6dbb510d32a5bf8b58df
aech046@gmail.com <aech046@gmail.com>
2023-12-11 09:11:27 +0000

    Created LICENSE.txt; Edited ResultPage and QuestionPage

--- client_code/QuestionPage/__init__.py
+++ client_code/QuestionPage/__init__.py
@@ -7,26 +7,34 @@ from ..ResultPage import ResultPage
 from ..ContactUsPage import ContactUsPage
 
 class QuestionPage(QuestionPageTemplate):
-  def __init__(self, **properties):
-    # Set Form properties and Data Bindings.
-    self.init_components(**properties)
+    def __init__(self, **properties):
+        # Set Form properties and Data Bindings.
+        self.init_components(**properties)
 
-    # Any code you write here will run before the form opens.
+        # Any code you write here will run before the form opens.
 
-  def radio_button_2_clicked(self, **event_args):
-    """This method is called when this radio button is selected"""
-    pass
+    def radio_button_2_clicked(self, **event_args):
+        """This method is called when this radio button is selected"""
+        pass
 
-  def link_1_click(self, **event_args):
-    """This method is called when the link is clicked"""
-    open_form('WelcomePage')
+    def link_1_click(self, **event_args):
+        open_form('WelcomePage')
 
-  def button_1_click(self, **event_args):
-    pass
+    def button_1_click(self, **event_args):
+        # Get the user's responses
+        response1 = self.text_box_1.text
+        response2 = self.text_box_2.text
+        response3 = self.text_box_3.text
 
-  def link_2_click(self, **event_args):
-    open_form('ContactUsPage')
+        # Call the Anvil server function to send responses to Jupyter notebook
+        result = anvil.server.call('predict_result', response1, response2, response3)
 
-  def button_2_click(self, **event_args):
-    """This method is called when the button is clicked"""
-    pass
+        # Open the ResultPage and pass the result as a parameter
+        open_form(ResultPage, result=result)
+
+    def link_2_click(self, **event_args):
+        open_form('ContactUsPage')
+
+    def button_2_click(self, **event_args):
+        """This method is called when the button is clicked"""
+        pass
--- client_code/ResultPage/__init__.py
+++ client_code/ResultPage/__init__.py
@@ -1,21 +1,18 @@
 from ._anvil_designer import ResultPageTemplate
 from anvil import *
-import anvil.server
-
-from ..WelcomePage import WelcomePage
-from ..ContactUsPage import ContactUsPage
 
 class ResultPage(ResultPageTemplate):
-  def __init__(self, **properties):
-    # Set Form properties and Data Bindings.
-    self.init_components(**properties)
+    def __init__(self, result, **properties):
+        # Set Form properties and Data Bindings.
+        self.init_components(**properties)
 
-    # Any code you write here will run before the form opens.
+        # Display the result in a label or handle it as needed
+        self.label_result.text = result
 
-  def link_1_click(self, **event_args):
-    """This method is called when the link is clicked"""
-    open_form("WelcomePage")
+    def link_1_click(self, **event_args):
+        """This method is called when the link is clicked"""
+        open_form("WelcomePage")
 
-  def link_2_click(self, **event_args):
-    """This method is called when the link is clicked"""
-    open_form('ContactUsPage')
+    def link_2_click(self, **event_args):
+        """This method is called when the link is clicked"""
+        open_form('ContactUsPage')
